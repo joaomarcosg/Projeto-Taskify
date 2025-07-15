6abb605ba4db34b8c7bc2746c3f2d417a3e809fe
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-07-15 17:21:06 -0300

    TEST: adiciona teste para o handleCreateTask

--- internal/api/tasks_handlers_test.go
+++ internal/api/tasks_handlers_test.go
@@ -1,1 +1,51 @@
 package api
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestCreateTask(t *testing.T) {
+	api := Application{}
+
+	payload := map[string]any{
+		"title":       "Learn TDD",
+		"description": "Get hands-on exp with TDD in Go",
+		"priority":    10,
+	}
+
+	body, err := json.Marshal(payload)
+	if err != nil {
+		t.Fatal("Fail to parse our request payload")
+	}
+
+	req := httptest.NewRequest("POST", "/api/v1/tasks", bytes.NewReader(body))
+	req.Header.Set("Content-Type", "application/json")
+
+	rec := httptest.NewRecorder()
+
+	handler := http.HandlerFunc(api.handleCreateTask)
+
+	handler.ServeHTTP(rec, req)
+
+	t.Logf("Rec body %s\n", rec.Body.Bytes())
+
+	if rec.Code != http.StatusCreated {
+		t.Errorf("Statuscode differs; got %d | want %d", rec.Code, http.StatusCreated)
+	}
+
+	var resBody map[string]any
+
+	err = json.Unmarshal(rec.Body.Bytes(), &resBody)
+	if err != nil {
+		t.Fatalf("Failed to parse response body: %s\n", err.Error())
+	}
+
+	if resBody["title"] != payload["title"] {
+		t.Errorf("title differs; got: %q | want: %q", resBody["title"], payload["title"])
+	}
+
+}
