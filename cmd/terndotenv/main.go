2c29fb4637ff994a2bfcb005e8fe30df613bf96f
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-07-15 13:54:46 -0300

    FEAT: adiciona funcionalidades da aplicação Teskify

---
+++ cmd/terndotenv/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+
+	cmd := exec.Command(
+		"tern",
+		"migrate",
+		"--migrations",
+		"./internal/store/pgstore/migrations",
+		"--config",
+		"./internal/store/pgstore/migrations/tern.conf",
+	)
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println("command execution failed:", err)
+		fmt.Println("output:", string(output))
+		return
+	}
+
+	fmt.Println("command executed sucessfully\n", string(output))
+
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/joaomarcosg/Projeto-Taskify
 
 go 1.24.2
+
+require (
+	github.com/go-chi/chi/v5 v5.2.2
+	github.com/joho/godotenv v1.5.1
+)
