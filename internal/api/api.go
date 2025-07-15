79dc306b193826f2663e75d0eccf7ac62414b39f
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-07-15 20:01:51 -0300

    FEAT: injeta camada de serviços no tipo Application

--- internal/api/api.go
+++ internal/api/api.go
@@ -1,7 +1,11 @@
 package api
 
-import "github.com/go-chi/chi/v5"
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/joaomarcosg/Projeto-Taskify/internal/services"
+)
 
 type Application struct {
-	Router *chi.Mux
+	Router      *chi.Mux
+	TaskService services.TaskService
 }
