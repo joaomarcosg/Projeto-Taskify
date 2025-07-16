b430f3b663c5a42eda83535960fe5d4c67c0c636
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-07-15 22:48:16 -0300

    FEAT: implementa métodos da interface TaskStore

--- internal/store/pgstore/pg_task_store.go
+++ internal/store/pgstore/pg_task_store.go
@@ -1,6 +1,11 @@
 package pgstore
 
-import "github.com/jackc/pgx/v5/pgxpool"
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joaomarcosg/Projeto-Taskify/internal/store"
+)
 
 type PGTaskStore struct {
 	Queries *Queries
@@ -12,4 +17,24 @@ func NewPGTaskStore(pool *pgxpool.Pool) PGTaskStore {
 		Queries: New(pool),
 		Pool:    pool,
 	}
+}
+
+func (pgs *PGTaskStore) CreateTask(ctx context.Context, title, description string, priority int32) (store.Task, error) {
+
+	task, err := pgs.Queries.CreateTask(ctx, CreateTaskParams{
+		Title:       title,
+		Description: description,
+		Priority:    priority,
+	})
+	if err != nil {
+		return store.Task{}, err
+	}
+	return store.Task{
+		Id:          task.ID,
+		Title:       task.Title,
+		Description: task.Description,
+		Priority:    task.Priority,
+		CreatedAt:   task.CreatedAt.Time,
+		UpdatedAt:   task.UpdatedAt.Time,
+	}, nil
 }
